59ad5b4b07dbc90c0cf247c1beb93f45b615f967
warrenshea <warren.shea@gmail.com>
2018-01-22 00:43:18 -0500

    Module 9: Adding routing

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
+
 import './css/style.css';
 
 import App from './components/App';
-
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
-render(<App/>,document.querySelector('#main'));
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>{/* Match cannot be a child of BrowserRouter....I guess? */}
+        {/* For the root */}
+        <Match exactly pattern='/' component={StorePicker} />
+        {/* For anything '/store' */}
+        <Match pattern='/store/:storeId' component={App} />
+        {/* For anything '/store' */}
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root/>,document.querySelector('#main'));
