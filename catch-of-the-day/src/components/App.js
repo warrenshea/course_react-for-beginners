b471ae32dde0bcf8e42240e775ab7cd301c99252
Warren Shea <warren.shea@gmail.com>
2018-02-10 00:21:20 -0500

    Module 16

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
 
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
 
     // getInitialState
     this.state = {
@@ -38,6 +39,16 @@ class App extends React.Component {
       fishes: sampleFishes
     });
   }
+
+  addToOrder (key) {
+    // update our state
+    const order = { ...this.state.order };
+    // update or add new number of fish ordered
+    order[key] = order[key] + 1 || 1;
+    // update state
+    this.setState({ order });
+  }
+
   render () {
     return (
       <div className='catch-of-the-day'>
@@ -47,7 +58,13 @@ class App extends React.Component {
             {
               Object
                 .keys(this.state.fishes)
-                .map(key => <Fish key={key} details={this.state.fishes[key]} />)
+                .map(key =>
+                  <Fish
+                    key={key}
+                    index={key}
+                    details={this.state.fishes[key]}
+                    addToOrder={this.addToOrder}
+                  />)
             }
           </ul>
         </div>
--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -6,16 +6,18 @@ class Fish extends React.Component {
     /* OLD WAY
     const details = this.props.details; */
     /* ES6 Destructuing Way */
-    const { details } = this.props;
+    const { details, index } = this.props;
+    const isAvailable = details.status === 'available';
+    const buttonText = isAvailable ? 'Add to order' : 'Sold out!';
     return (
       <li className='menu-fish'>
         <img src={details.image} alt={details.name} />
-        <h3 className="fish-name">
+        <h3 className='fish-name'>
           { details.name }
           <span className='price'>{formatPrice(details.price)}</span>
         </h3>
         <p>{details.desc}</p>
-        <button>Add to order</button>
+        <button disabled={!isAvailable} onClick={() => this.props.addToOrder(index)}>{ buttonText }</button>
       </li>
     );
   }
