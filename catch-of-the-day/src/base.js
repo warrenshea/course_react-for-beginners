e5d0a579827d162b20a0aad358654c80377054f0
Warren Shea <warren.shea@gmail.com>
2018-02-10 15:07:19 -0500

    Module 18

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: 'AIzaSyCYgnw_pVFF9_VJ_t42zq0sThhtici5ffw',
+  authDomain: 'react-for-beginners-bbddc.firebaseapp.com',
+  databaseURL: 'https://react-for-beginners-bbddc.firebaseio.com'
+});
+
+export default base;
